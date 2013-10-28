c3ec8205711d2f33672769d71d7e714f39a78ffd
Kat Reinhart <kat.reinhart@gmail.com>
2013-10-28 10:42:01 -0700

    Worked through exercises chapters 4-7

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,11 +3,21 @@ require 'spec_helper'
 describe "StaticPages" do
   
   subject { page }
+  
+  shared_examples_for "all static pages" do
+  	it { should have_selector('h1', text: heading) }
+  	it { should have_title(full_title(page_title)) }
+  end
+  
+  
+
+
   describe "Home page" do
     before { visit root_path }
+	let(:page_title) { '' }
+	let(:heading) { 'Home' }
 	
-	it { should have_content('My Project') }
-	it { should have_title(full_title('')) }
+	it_should_behave_like "all static pages"
 	it { should_not have_title('| Home') }
 	
 	describe "for signed-in users" do
@@ -29,25 +39,44 @@ describe "StaticPages" do
 
   describe "Help page" do
     before { visit help_path }
-    
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+  
+    let(:page_title) { 'Help' }
+    let(:heading) { 'Help' }
+    it_should_behave_like "all static pages"
     
   end
   
   describe "About page" do
     before { visit about_path }
-    
-    it { should have_content('About') }
-    it { should have_title(full_title('About Me')) }
+  	let(:page_title) { 'About' }
+  	let(:heading) { 'About' }
+  	it_should_behave_like "all static pages"
+  	
   end
   
   
   describe "Contact page" do
 	before { visit contact_path }
+	let(:page_title) { 'Contact' }
+	let(:heading) { 'Contact' }
 	
-	it { should have_content('Contact') }
-	it { should have_title(full_title('Contact')) }
+	it_should_behave_like "all static pages"
+	
+  end
+
+  it "should have the right links on the layout" do
+  	visit root_path
+  	click_link "About"
+  	expect(page).to have_title(full_title('About Me'))
+  	click_link "Help"
+  	expect(page).to have_title(full_title('Help'))
+  	click_link "Contact"
+  	expect(page).to have_title(full_title('Contact'))
+  	click_link "Home"
+  	click_link "Sign up now!"
+  	expect(page).to have_content("Sign up")
+  	click_link "sample app"
+  	expect(page).to have_content("Home")
   end
 
 end
